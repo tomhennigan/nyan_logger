da2c074970857e8070fb15b1096663e788b92c8d
Tom Hennigan <tomhennigan@gmail.com>
2013-07-04 09:46:03 +0000

    Bump version to 0.2.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='nyan_logger',
-      version='0.1',
+      version='0.2',
       description='A nyan cat log formatter for the python logging module.',
       url='https://github.com/tomhennigan/nyan_logger',
       author='Tom Hennigan, Louise Deason',
